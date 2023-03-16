31be3b884b81fb00ae3eaf781580656aeb3090b1
bruniin <brunovinicius2002@hotmail.com>
2023-03-16 11:39:43 -0300

    UI changes

--- to_do_list.py
+++ to_do_list.py
@@ -6,7 +6,7 @@ corbg = "#242323"
 def add_item():
     item = item_entry.get()
     if item:
-        item_list.insert(END, item)
+        item_list.insert(END, " - " + item)
         item_entry.delete(0, END)
 
 def remove_item():
@@ -16,42 +16,43 @@ def remove_item():
 def completed():
     selec = item_list.curselection()
     if selec:
+        completed_list["state"] = "normal"
         completed_list.insert(END, item_list.get(selec))
         item_list.delete(selec[0])
+        completed_list["state"] = "disabled"
 
 
 
 main = Tk()
 main.title("to-do list")
-main.geometry("800x500+700+300")
+main.geometry("665x550+700+300")
 main.config(bg = "#242323")
 #main.resizable(width = False, height = False)
 
 #campo para adicionar um item
-item_entry = Entry(main, bg=corbg, fg="white")
-item_entry.pack()
+item_entry = Entry(main, bg=corbg, fg="white", width=30, font="calibri 12", relief="solid")
+item_entry.place(x=30, y=30)
 
 #botão para adicionar o item
-add_button = Button(main, text="Add Task", command=add_item, bg=corbg, fg="white", relief="solid")
-add_button.pack()
-
+add_button = Button(main, text="Add Task", command=add_item, bg=corbg, fg="white", relief="solid", font="arial 10 bold")
+add_button.place(x=30, y=60)
 
 #lista de itens a fazer
-item_list = Listbox(main, height=20, width=40, bg=corbg, fg="white", relief="solid", highlightthickness=2)
+item_list = Listbox(main, height=15, width=35, bg=corbg, fg="white", relief="solid", highlightthickness=2, font="calibri")
 item_list.configure(highlightbackground="purple")
-item_list.pack()
+item_list.place(x=30, y=160)
 
 #botão para remover itens
-remove_button = Button(main, text="Remove Task", command=remove_item, bg=corbg, fg="white", relief="solid")
-remove_button.pack()
+remove_button = Button(main, text="Remove Task", command=remove_item, bg=corbg, fg="white", relief="solid", font="arial 10 bold")
+remove_button.place(x=30, y=475)
 
 #botão para completar tarefas
-completed_button = Button(main, text="Completed", command=completed, bg=corbg, fg="white", relief="solid")
-completed_button.pack()
+completed_button = Button(main, text="Completed", command=completed, bg=corbg, fg="white", relief="solid", font="arial 10 bold")
+completed_button.place(x=235, y=475)
 
 #lista de tarefas completadas
-completed_list = Listbox(main, height=20, width=40, bg=corbg, fg="white", relief="solid", highlightthickness=2)
+completed_list = Listbox(main, height=15, width=35, bg=corbg, fg="white", relief="solid", highlightthickness=2, font="calibri")
 completed_list.configure(highlightbackground="purple")
-completed_list.pack()
+completed_list.place(x=350, y=160)
 
 main.mainloop()
